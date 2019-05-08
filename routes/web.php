3d64f4a7ee801e1ac660bba5be80cc3d44925e89
drLev <drlev52@gmail.com>
2019-05-08 21:18:51 +0300

    layouts

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'Home@index')->name('home');
 
-Route::get('/', function () {
-    return view('blog.index');
-})->name('home');
+Route::get('/about', 'About@index')->name('about');
 
-Route::get('/about', function () {
-    return view('blog.about');
-})->name('about');
+Route::get('/post', 'Post@index')->name('post');
 
-Route::get('/post', function () {
-    return view('blog.post');
-})->name('post');
-
-Route::get('/contact', function () {
-    return view('blog.contact');
-})->name('contact');
+Route::get('/contact', 'Contact@index')->name('contact');
